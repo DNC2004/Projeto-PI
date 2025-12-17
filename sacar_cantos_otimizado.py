d9cf7ea78955fe994a077da92f82bdaee24ba5cb
D_N_C_2004 <dinisncarraca2@gmail.com>
2025-12-17 19:34:14 +0000

    Update sacar_cantos_otimizado.py
    Comentários atualizados

--- sacar_cantos_otimizado.py
+++ sacar_cantos_otimizado.py
@@ -13,13 +13,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-#caminho_img = 'Imagens/jogo_stor.jpg'
-caminho_img = 'Imagens/jogo_distorced.jpg' ## ATUAL
-#caminho_img = 'Imagens/jogo_perp.jpeg'
-#caminho_img = 'Imagens/jogo_blackgorund.jpg'
-#caminho_img = 'Imagens/jogo_scuro.jpg'
-#caminho_img = 'Imagens/jogo_purp.jpg'
-#caminho_img = 'Imagens/jogo_red.jpg'
+#caminho_img = 'Imagens/jogo_stor.jpg' # Não funciona
+#caminho_img = 'Imagens/jogo_distorced.jpg' ## ATUAL / Funciona Tudo
+#caminho_img = 'Imagens/jogo_perp.jpeg' # Funciona Tudo
+#caminho_img = 'Imagens/jogo_blackgorund.jpg' # Funciona Tudo
+#caminho_img = 'Imagens/jogo_scuro.jpg'  # Funciona Tudo
+#caminho_img = 'Imagens/jogo_purp.jpg' # Funciona Tudo
+caminho_img = 'Imagens/jogo_red.jpg' # Funciona Tudo
 
 
 # --- Load image ---
