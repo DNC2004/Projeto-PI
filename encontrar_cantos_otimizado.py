9453edd0464345d870692ab50aec0f072eb52418
D_N_C_2004 <dinisncarraca2@gmail.com>
2026-01-19 17:33:46 +0000

    Agora é uma função

--- encontrar_cantos_otimizado.py
+++ encontrar_cantos_otimizado.py
@@ -3,315 +3,320 @@ import matplotlib.pyplot as plt
 import cv2
 
 # Encontra o tabuleiro de jogo na fotografia
-caminho_img = 'Imagens/jogo_stor.jpg' # Funciona tudo
-#caminho_img = 'Imagens/jogo_distorced.jpg' ## ATUAL / Funciona Tudo
+#caminho_img = 'Imagens/jogo_stor.jpg' # Funciona tudo
+caminho_img = 'Imagens/jogo_distorced.jpg' ## ATUAL / Funciona Tudo
 #caminho_img = 'Imagens/jogo_perp.jpeg' # Funciona Tudo
 #caminho_img = 'Imagens/jogo_blackgorund.jpg' # Funciona Tudo
 #caminho_img = 'Imagens/jogo_scuro.jpg'  # Funciona Tudo
 #caminho_img = 'Imagens/jogo_purp.jpg' # Funciona Tudo
 #caminho_img = 'Imagens/jogo_red.jpg' # Funciona Tudo
 
+def encontrar_cantos(img_path):
+    """ 
+    1. PRÉ-PROCESSAMENTO
+    """
 
-""" 
-1. PRÉ-PROCESSAMENTO
-"""
+    img = cv2.imread(img_path)
 
-img = cv2.imread(caminho_img)
+    #Preparar Imagem / Melhora a robustez
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    escala = 1200 / max(img_rgb.shape[:2])
+    img_rgb = cv2.resize(img_rgb, None, fx=escala, fy=escala, interpolation=cv2.INTER_AREA)
 
-#Preparar Imagem / Melhora a robustez
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-escala = 1200 / max(img_rgb.shape[:2])
-img_rgb = cv2.resize(img_rgb, None, fx=escala, fy=escala, interpolation=cv2.INTER_AREA)
+    # Converter para HSV
+    img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
 
-# Converter para HSV
-img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
+    # Segmentar vermelho
+    mask_r1 = cv2.inRange(img_hsv, (0, 80, 60), (10, 255, 255))
+    mask_r2 = cv2.inRange(img_hsv, (170, 80, 60), (180, 255, 255))
+    mask_red = cv2.bitwise_or(mask_r1, mask_r2)
 
-# Segmentar vermelho
-mask_r1 = cv2.inRange(img_hsv, (0, 80, 60), (10, 255, 255))
-mask_r2 = cv2.inRange(img_hsv, (170, 80, 60), (180, 255, 255))
-mask_red = cv2.bitwise_or(mask_r1, mask_r2)
+    # Segmentar branco
+    mask_white = cv2.inRange(img_hsv, (0, 0, 160), (180, 80, 255))
 
-# Segmentar branco
-mask_white = cv2.inRange(img_hsv, (0, 0, 160), (180, 80, 255))
+    # Remover Ruído
+    mask_red = cv2.medianBlur(mask_red, 5)
+    mask_white = cv2.medianBlur(mask_white, 5)
 
-# Remover Ruído
-mask_red = cv2.medianBlur(mask_red, 5)
-mask_white = cv2.medianBlur(mask_white, 5)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+    # Expandir as regiões para detetar melhor o tabuleiro
+    mask_r_d = cv2.dilate(mask_red, kernel, iterations = 1)
+    mask_w_d = cv2.dilate(mask_white, kernel, iterations = 1)
 
-# Expandir as regiões para detetar melhor o tabuleiro
-mask_r_d = cv2.dilate(mask_red, kernel, iterations = 1)
-mask_w_d = cv2.dilate(mask_white, kernel, iterations = 1)
+    inter = cv2.bitwise_and(mask_r_d, mask_w_d) # Área com overlap
 
-inter = cv2.bitwise_and(mask_r_d, mask_w_d) # Área com overlap
-
-# Regiões vermelhas e brancas
-num_r, labels_r = cv2.connectedComponents(mask_r_d, connectivity = 8)
-num_w, labels_w = cv2.connectedComponents(mask_w_d)
- 
-#  Ficamos apenas com os componentes que que se intercetam
-# Evita contabilizar regiões com as mesmas cores que o tabuleiro mas que não sejam o tabuleiro
-ids_r = np.unique(labels_r[inter > 0])
-ids_w = np.unique(labels_w[inter > 0])
- 
-# Remover a label
-ids_r = ids_r[ids_r != 0]
-ids_w = ids_w[ids_w != 0]
-
-""" 
-2. ENCONTRAR A ÁREA DE JOGO
-"""
-""""""
-
-# Máscara nos componentes que se toquem 
-mask_r_touch = np.zeros_like(labels_r, dtype=np.uint8)
-for rid in ids_r: 
-    mask_r_touch |= (labels_r == rid)
+    # Regiões vermelhas e brancas
+    num_r, labels_r = cv2.connectedComponents(mask_r_d, connectivity = 8)
+    num_w, labels_w = cv2.connectedComponents(mask_w_d)
     
-mask_r_touch = (mask_r_touch * 255).astype(np.uint8)
- 
-mask_w_touch = np.zeros_like(labels_w, dtype=np.uint8)
-for wid in ids_w: 
-    mask_w_touch |= (labels_w == wid)
+    #  Ficamos apenas com os componentes que que se intercetam
+    # Evita contabilizar regiões com as mesmas cores que o tabuleiro mas que não sejam o tabuleiro
+    ids_r = np.unique(labels_r[inter > 0])
+    ids_w = np.unique(labels_w[inter > 0])
     
-mask_w_touch = (mask_w_touch * 255).astype(np.uint8)
- 
-# Masc. com a área do tabuleiro
-mask_tiles_touch = cv2.bitwise_or(mask_r_touch, mask_w_touch)
- 
-mask = cv2.erode(mask_tiles_touch, kernel, iterations=1)
- 
-# Contornos
-contornos,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-if not contornos:
-    raise ValueError("Nenhum contorno encontrado.")
+    # Remover a label
+    ids_r = ids_r[ids_r != 0]
+    ids_w = ids_w[ids_w != 0]
 
-# Maior contorno == Tabuleiro
-largest = max(contornos, key=cv2.contourArea)
-perimetro = cv2.arcLength(largest, True)
-approx = cv2.approxPolyDP(largest, 0.01 * perimetro, True)
- 
-mask_refinada = np.zeros_like(mask)
-cv2.drawContours(mask_refinada, [approx], -1, 255, thickness=-1)
+    """ 
+    2. ENCONTRAR A ÁREA DE JOGO
+    """
+    """"""
 
-contornos,_ = cv2.findContours(mask_refinada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contorno = max(contornos, key=cv2.contourArea)
+    # Máscara nos componentes que se toquem 
+    mask_r_touch = np.zeros_like(labels_r, dtype=np.uint8)
+    for rid in ids_r: 
+        mask_r_touch |= (labels_r == rid)
+        
+    mask_r_touch = (mask_r_touch * 255).astype(np.uint8)
+    
+    mask_w_touch = np.zeros_like(labels_w, dtype=np.uint8)
+    for wid in ids_w: 
+        mask_w_touch |= (labels_w == wid)
+        
+    mask_w_touch = (mask_w_touch * 255).astype(np.uint8)
+    
+    # Masc. com a área do tabuleiro
+    mask_tiles_touch = cv2.bitwise_or(mask_r_touch, mask_w_touch)
+    
+    mask = cv2.erode(mask_tiles_touch, kernel, iterations=1)
+    
+    # Contornos
+    contornos,_ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contornos:
+        raise ValueError("Nenhum contorno encontrado.")
 
-# Pontos da borda do tabuleiro
-epsilon = 0.01 * cv2.arcLength(contorno, True)
-approx = cv2.approxPolyDP(contorno, epsilon, True)
-points = approx.reshape(-1, 2)
+    # Maior contorno == Tabuleiro
+    largest = max(contornos, key=cv2.contourArea)
+    perimetro = cv2.arcLength(largest, True)
+    approx = cv2.approxPolyDP(largest, 0.01 * perimetro, True)
+    
+    mask_refinada = np.zeros_like(mask)
+    cv2.drawContours(mask_refinada, [approx], -1, 255, thickness=-1)
 
-segments = []
-for i in range(len(points)):
-    p1 = points[i]
-    p2 = points[(i + 1) % len(points)]
-    length = np.linalg.norm(p1 - p2)
-    segments.append((length, p1, p2))
+    contornos,_ = cv2.findContours(mask_refinada, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contorno = max(contornos, key=cv2.contourArea)
 
-# Escolhe as quatro maiores arestas -- Área possível do tabuleiro 
-segments.sort(key=lambda x: x[0], reverse=True)
-top4 = segments[:4]
+    # Pontos da borda do tabuleiro
+    epsilon = 0.01 * cv2.arcLength(contorno, True)
+    approx = cv2.approxPolyDP(contorno, epsilon, True)
+    points = approx.reshape(-1, 2)
 
-# Determina o ponto de interseção entre duas retas
-def line_intersection(p1, p2, p3, p4):
-    x1, y1 = p1
-    x2, y2 = p2
-    x3, y3 = p3
-    x4, y4 = p4
+    segments = []
+    for i in range(len(points)):
+        p1 = points[i]
+        p2 = points[(i + 1) % len(points)]
+        length = np.linalg.norm(p1 - p2)
+        segments.append((length, p1, p2))
 
-    denom = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
-    if abs(denom) < 1e-6:
-        return None
-    px = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / denom
-    py = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / denom
-    return np.array([px, py])
+    # Escolhe as quatro maiores arestas -- Área possível do tabuleiro 
+    segments.sort(key=lambda x: x[0], reverse=True)
+    top4 = segments[:4]
 
-# Interseções entre os maiores pares de retas
-intersections = []
-for i in range(len(top4)):
-    for j in range(i+1, len(top4)):
-        inter = line_intersection(top4[i][1], top4[i][2], top4[j][1], top4[j][2])
-        if inter is not None:
-            intersections.append(inter)
+    # Determina o ponto de interseção entre duas retas
+    def line_intersection(p1, p2, p3, p4):
+        x1, y1 = p1
+        x2, y2 = p2
+        x3, y3 = p3
+        x4, y4 = p4
 
-# Encontrar os centroide 
-M = cv2.moments(approx)
-cx = int(M["m10"] / M["m00"])
-cy = int(M["m01"] / M["m00"])
-centroid = np.array([cx, cy])
+        denom = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+        if abs(denom) < 1e-6:
+            return None
+        px = ((x1*y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4)) / denom
+        py = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / denom
+        return np.array([px, py])
 
-# Escolher as interseções mais perto deste
-if len(intersections) > 4:
-    intersections = sorted(intersections, key=lambda p: np.linalg.norm(p - centroid))[:4]
+    # Interseções entre os maiores pares de retas
+    intersections = []
+    for i in range(len(top4)):
+        for j in range(i+1, len(top4)):
+            inter = line_intersection(top4[i][1], top4[i][2], top4[j][1], top4[j][2])
+            if inter is not None:
+                intersections.append(inter)
 
-# Desenhar a deteção
-overlay = img_rgb.copy()
+    # Encontrar os centroide 
+    M = cv2.moments(approx)
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
+    centroid = np.array([cx, cy])
 
-for pt in intersections:
-    cv2.circle(overlay, tuple(np.int32(pt)), 6, (0,255,0), -1)
+    # Escolher as interseções mais perto deste
+    if len(intersections) > 4:
+        intersections = sorted(intersections, key=lambda p: np.linalg.norm(p - centroid))[:4]
 
-cv2.circle(overlay, (cx, cy), 6, (255,0,0), -1)
+    # Desenhar a deteção
+    overlay = img_rgb.copy()
 
-for (_, p1, p2) in top4:
-    cv2.line(overlay, tuple(p1), tuple(p2), (0,0,255), 10)
+    for pt in intersections:
+        cv2.circle(overlay, tuple(np.int32(pt)), 6, (0,255,0), -1)
 
-alpha = 0.6
-output = cv2.addWeighted(img_rgb, 1 - alpha, overlay, alpha, 0)
+    cv2.circle(overlay, (cx, cy), 6, (255,0,0), -1)
 
-""" 3. TRANSFORMAR PARA A PERSPETIVA """
-""""""
-def order_points(pts):
-    rect = np.zeros((4, 2), dtype="float32")
+    for (_, p1, p2) in top4:
+        cv2.line(overlay, tuple(p1), tuple(p2), (0,0,255), 10)
 
-    s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]   # Top-Left
-    rect[2] = pts[np.argmax(s)]   # Bottom-Right
+    alpha = 0.6
+    output = cv2.addWeighted(img_rgb, 1 - alpha, overlay, alpha, 0)
 
-    diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]  # Top-Right
-    rect[3] = pts[np.argmax(diff)]  # Bottom-Left
+    """ 3. TRANSFORMAR PARA A PERSPETIVA """
+    """"""
+    def order_points(pts):
+        rect = np.zeros((4, 2), dtype="float32")
 
-    return rect
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]   # Top-Left
+        rect[2] = pts[np.argmax(s)]   # Bottom-Right
 
-# Converter lista de interseções para matriz e ordenar
-corners = np.array(intersections, dtype="float32")
-rect = order_points(corners)
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]  # Top-Right
+        rect[3] = pts[np.argmax(diff)]  # Bottom-Left
 
-# Perspetiva final
-dst = np.array([
-    [100, 100],
-    [600, 100],
-    [600, 600],
-    [100, 600]
-], dtype="float32")
+        return rect
 
-M = cv2.getPerspectiveTransform(rect, dst)
+    # Converter lista de interseções para matriz e ordenar
+    corners = np.array(intersections, dtype="float32")
+    rect = order_points(corners)
 
-mask_3c = cv2.merge([mask_refinada]*3)
-masked_img = cv2.bitwise_and(img_rgb, mask_3c)
+    # Perspetiva final
+    dst = np.array([
+        [100, 100],
+        [600, 100],
+        [600, 600],
+        [100, 600]
+    ], dtype="float32")
 
-x_min = int(np.min(rect[:, 0]))
-x_max = int(np.max(rect[:, 0]))
-y_min = int(np.min(rect[:, 1]))
-y_max = int(np.max(rect[:, 1]))
+    M = cv2.getPerspectiveTransform(rect, dst)
 
-cropped_masked = masked_img[y_min:y_max, x_min:x_max]
+    mask_3c = cv2.merge([mask_refinada]*3)
+    masked_img = cv2.bitwise_and(img_rgb, mask_3c)
 
-size = max(y_max - y_min, x_max - x_min)
-dst_tight = np.array([
-    [0, 0],
-    [size, 0],
-    [size, size],
-    [0, size]
-], dtype="float32")
+    x_min = int(np.min(rect[:, 0]))
+    x_max = int(np.max(rect[:, 0]))
+    y_min = int(np.min(rect[:, 1]))
+    y_max = int(np.max(rect[:, 1]))
 
-rect_cropped = rect.copy()
-rect_cropped[:, 0] -= x_min
-rect_cropped[:, 1] -= y_min
+    cropped_masked = masked_img[y_min:y_max, x_min:x_max]
 
-# Transformar para a perspectiva final
-M_tight = cv2.getPerspectiveTransform(rect_cropped, dst_tight)
-warped_tight = cv2.warpPerspective(cropped_masked, M_tight, (size, size))
-warped_export = cv2.cvtColor(warped_tight, cv2.COLOR_BGR2RGB)
+    size = max(y_max - y_min, x_max - x_min)
+    dst_tight = np.array([
+        [0, 0],
+        [size, 0],
+        [size, size],
+        [0, size]
+    ], dtype="float32")
 
-cv2.imwrite("imagem_final.png", warped_export)
+    rect_cropped = rect.copy()
+    rect_cropped[:, 0] -= x_min
+    rect_cropped[:, 1] -= y_min
 
-""" 4. PLOTS FINAIS """
-""""""
+    # Transformar para a perspectiva final
+    M_tight = cv2.getPerspectiveTransform(rect_cropped, dst_tight)
+    warped_tight = cv2.warpPerspective(cropped_masked, M_tight, (size, size))
+    warped_export = cv2.cvtColor(warped_tight, cv2.COLOR_BGR2RGB)
 
-# Plots com o processo até chegar à imagem final  
-plt.figure(figsize=(20, 12))
+    cv2.imwrite("imagem_final.png", warped_export)
 
-plt.subplot(2, 4, 1)
-plt.imshow(img_rgb)
-plt.title("Original")
-plt.axis("off")
+    """ 4. PLOTS FINAIS """
+    """"""
 
-plt.subplot(2, 4, 2)
-plt.imshow(mask_red, cmap="gray")
-plt.title("Máscara Vermelha")
-plt.axis("off")
+    # Plots com o processo até chegar à imagem final  
+    plt.figure(figsize=(20, 12))
 
-"""
-## DEBUG
-red_export = cv2.cvtColor(mask_red, cv2.COLOR_BGR2RGB)
-cv2.imwrite("masc_red.png", red_export)
-"""
+    plt.subplot(2, 4, 1)
+    plt.imshow(img_rgb)
+    plt.title("Original")
+    plt.axis("off")
 
-plt.subplot(2, 4, 3)
-plt.imshow(mask_white, cmap="gray")
-plt.title("Máscara Branca")
-plt.axis("off")
+    plt.subplot(2, 4, 2)
+    plt.imshow(mask_red, cmap="gray")
+    plt.title("Máscara Vermelha")
+    plt.axis("off")
 
-"""
-## DEBUG
-white_export = cv2.cvtColor(mask_white, cv2.COLOR_BGR2RGB)
-cv2.imwrite("masc_white.png", white_export)
-"""
+    """
+    ## DEBUG
+    red_export = cv2.cvtColor(mask_red, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("masc_red.png", red_export)
+    """
+
+    plt.subplot(2, 4, 3)
+    plt.imshow(mask_white, cmap="gray")
+    plt.title("Máscara Branca")
+    plt.axis("off")
+
+    """
+    ## DEBUG
+    white_export = cv2.cvtColor(mask_white, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("masc_white.png", white_export)
+    """
 
 
-plt.subplot(2, 4, 4)
-plt.imshow(mask_tiles_touch)
-plt.title("Área de Jogo")
-plt.axis("off")
+    plt.subplot(2, 4, 4)
+    plt.imshow(mask_tiles_touch)
+    plt.title("Área de Jogo")
+    plt.axis("off")
 
-"""
-area_jogo = cv2.cvtColor(mask_tiles_touch, cv2.COLOR_BGR2RGB)
-cv2.imwrite("area_jogo.png", area_jogo)
-"""
+    """
+    area_jogo = cv2.cvtColor(mask_tiles_touch, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("area_jogo.png", area_jogo)
+    """
 
-plt.subplot(2, 4, 5)
-plt.imshow(mask_refinada)
-plt.title("Área de jogo rough")
-plt.axis("off")
+    plt.subplot(2, 4, 5)
+    plt.imshow(mask_refinada)
+    plt.title("Área de jogo rough")
+    plt.axis("off")
 
-"""
-area_jogo_dura = cv2.cvtColor(mask_refinada, cv2.COLOR_BGR2RGB)
-cv2.imwrite("area_jogo_dura.png", area_jogo_dura)
-"""
+    """
+    area_jogo_dura = cv2.cvtColor(mask_refinada, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("area_jogo_dura.png", area_jogo_dura)
+    """
 
-plt.subplot(2, 4, 6)
-# Overlay with intersections
-overlay = img_rgb.copy()
-for pt in intersections:
-    cv2.circle(overlay, tuple(np.int32(pt)), 6, (0,255,0), -1)
-cv2.circle(overlay, (cx, cy), 6, (255,0,0), -1)
+    plt.subplot(2, 4, 6)
+    # Overlay with intersections
+    overlay = img_rgb.copy()
+    for pt in intersections:
+        cv2.circle(overlay, tuple(np.int32(pt)), 6, (0,255,0), -1)
+    cv2.circle(overlay, (cx, cy), 6, (255,0,0), -1)
 
-for (_, p1, p2) in top4:
-    cv2.line(overlay, tuple(p1), tuple(p2), (0,0,255), 2)
+    for (_, p1, p2) in top4:
+        cv2.line(overlay, tuple(p1), tuple(p2), (0,0,255), 2)
 
-alpha = 0.6
-output = cv2.addWeighted(img_rgb, 1 - alpha, overlay, alpha, 0)
-plt.imshow(output)
-plt.title("Overlay com interseções")
-plt.axis("off")
+    alpha = 0.6
+    output = cv2.addWeighted(img_rgb, 1 - alpha, overlay, alpha, 0)
+    plt.imshow(output)
+    plt.title("Overlay com interseções")
+    plt.axis("off")
 
-"""
-cantos_export = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-cv2.imwrite("cantos_find.png", cantos_export)
-"""
+    """
+    cantos_export = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("cantos_find.png", cantos_export)
+    """
 
-plt.subplot(2, 4, 7)
-plt.imshow(masked_img)
-plt.title("Imagem Original Apenas Área Detectada")
-plt.axis("off")
+    plt.subplot(2, 4, 7)
+    plt.imshow(masked_img)
+    plt.title("Imagem Original Apenas Área Detectada")
+    plt.axis("off")
 
-"""
-img_final_area = cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB)
-cv2.imwrite("img_final_area.png", img_final_area)
-"""
+    """
+    img_final_area = cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("img_final_area.png", img_final_area)
+    """
 
-plt.subplot(2, 4, 8)
-plt.imshow(warped_tight)
-plt.title("Imagem Final Warp sem Bordas")
-plt.axis("off")
+    plt.subplot(2, 4, 8)
+    plt.imshow(warped_tight)
+    plt.title("Imagem Final Warp sem Bordas")
+    plt.axis("off")
 
-"""
-img_final_warp = cv2.cvtColor(warped_tight, cv2.COLOR_BGR2RGB)
-cv2.imwrite("img_final_warp.png", img_final_warp)
-"""
+    """
+    img_final_warp = cv2.cvtColor(warped_tight, cv2.COLOR_BGR2RGB)
+    cv2.imwrite("img_final_warp.png", img_final_warp)
+    """
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+    
+    return warped_export
+
+if __name__ == "__main__":
+    encontrar_cantos(caminho_img)
